2388d141a58a9d777833aa28daec77e6c1bf74c4
Griffin Johnson <cougargriff@gmail.com>
2018-01-31 09:59:15 -0800

    more strassen

--- MatrixProduct.java
+++ MatrixProduct.java
@@ -61,12 +61,16 @@ public class MatrixProduct
 		else
 		{
 			int[][] s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
-			s1 = matrixDiff(B, startrowB, startcolB + n / 2, B, startrowB + n / 2, startcolB + n / 2); // s1 = b12 - b22
-			s2 = matrixAdd(A, startrowA, startcolA, A, startrowA, startcolA + n / 2); // s2 = a11 + a12
-			s3 = matrixAdd(A, startrowA + n / 2, startcolA, A, startrowA + n / 2, startcolA + n / 2); // s3 = a21 + a22
-			s4 = matrixDiff(B, startrowB + n / 2, startcolB, B, startrowB, startcolB); // s4 = b21 - b11
-			s5 = matrixAdd(A, startrowA, startcolA, A, startrowA + n / 2, startcolA + n / 2); // s5 = a11 + a22
-			s6 = 
+			s1  = matrixDiff(B, startrowB, startcolB + n / 2, B, startrowB + n / 2, startcolB + n / 2); // s1 = b12 - b22
+			s2  = matrixAdd(A, startrowA, startcolA, A, startrowA, startcolA + n / 2);                  // s2 = a11 + a12
+			s3  = matrixAdd(A, startrowA + n / 2, startcolA, A, startrowA + n / 2, startcolA + n / 2);  // s3 = a21 + a22
+			s4  = matrixDiff(B, startrowB + n / 2, startcolB, B, startrowB, startcolB);                 // s4 = b21 - b11
+			s5  = matrixAdd(A, startrowA, startcolA, A, startrowA + n / 2, startcolA + n / 2);          // s5 = a11 + a22
+			s6  = matrixAdd(B, startrowB, startcolB, B, startrowB + n / 2, startcolB + n / 2);          // s6 = b11 + b22
+			s7  = matrixDiff(A, startrowA, startcolA + n / 2, A, startrowA + n / 2, startcolA + n / 2); // s7 = a12 - a22
+			s8  = matrixAdd(B, startrowB + n / 2, startcolB, B, startrowB + n / 2, startcolB + n / 2);  // s8 = b21 + b22
+			s9  = matrixDiff(A, startrowA, startcolA, A, startrowA + n / 2, startcolA);                 // s9 = a11 - a21
+			s10 = matrixAdd(B, startrowB, startcolB, B, startrowB, startcolB + n / 2);                  // s10 = b11 + b12
 
 
 		}
