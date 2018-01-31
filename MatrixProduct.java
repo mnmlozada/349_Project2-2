21e3c3302d7dd9fb5fbfd0b4b5d2a8cd2dd4eb39
Griffin Johnson <cougargriff@gmail.com>
2018-01-30 22:11:26 -0800

    Added new parameters for strassen

--- MatrixProduct.java
+++ MatrixProduct.java
@@ -10,53 +10,46 @@ public class MatrixProduct
 		int aCol = A[0].length;
 		int bRow = B.length;
 		int bCol = B[0].length;
-		
+
 		//checks that each matrix is a square and that they equal each other
-		if(aRow != aCol && bRow != bCol && aCol != bRow) 
+		if(aRow != aCol && bRow != bCol && aCol != bRow)
 		{
 
 			throw new IllegalArgumentException("Invalid Matrices");
-			
+
 		}
-		
+
 		if(!(IsPowerOfTwo(aRow)))
 		{
-			
+
 			throw new IllegalArgumentException("Invalid Matrices");
 		}
 	}
-	
+
 	// determines if number is a power of 2
 	private static boolean IsPowerOfTwo(int x)
 	{
 		return (x != 0) && ((x & (x - 1)) == 0);
 	}
-	
+
 	public static int[][] matrixProduct_DAC(int[][] A, int[][] B)
 	{
-		
+
 	}
-	
+
 	private static int[][] matrixProduct_DAC()
 	{
-		
+
 	}
-	
-	public static int[][] matrixProduct_Strassen(int[][] A, int[][] B)
+
+	public static int[][] matrixProduct_Strassen(int[][] A, int rowA, int colA, int[][] B, int rowB, int colB)
 	{
-		
+
 	}
-	
-	private static int[][] matrixProduct_Strassen()
+
+	private static int[][] matrixProduct_Strassen(int[][] A, int rowA, int colA, int[][] B, int rowB, int colB)
 	{
-		
+
 	}
 
 }
-
-
-
-
-
-
-
